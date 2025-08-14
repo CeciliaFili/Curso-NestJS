4747f899d58eb9ac11d767930cb1cb902288eb4f
Cecilia Fili <cecifili@Cecilias-MacBook-Air.local>
2025-08-14 08:25:41 +0200

    feat: validation getTasksWithFilters

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,6 +1,12 @@
+import { IsEnum, IsOptional, IsString } from 'class-validator';
 import { TaskStatus } from '../task.model';
 
 export class GetTasksFilterDto {
+  @IsOptional()
+  @IsString()
   search?: TaskStatus;
+
+  @IsOptional()
+  @IsEnum(TaskStatus)
   status?: string;
 }
