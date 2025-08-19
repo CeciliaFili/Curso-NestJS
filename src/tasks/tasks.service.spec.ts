a82e8c8695aba7e61e02ed667dfbf70823f80444
Cecilia Fili <cecifili@Cecilias-MacBook-Air.local>
2025-08-19 16:17:12 +0200

    testing getTaskById

--- src/tasks/tasks.service.spec.ts
+++ src/tasks/tasks.service.spec.ts
@@ -41,52 +41,72 @@ describe('TasksService', () => {
       expect(result).toEqual([]);
       expect(tasksRepository.createQueryBuilder).toHaveBeenCalled();
     });
+    it('returns tasks filtered by status and search', async () => {
+      const tasks = [
+        {
+          id: '1',
+          title: 'Tarea 1',
+          description: 'Desc 1',
+          status: 'OPEN',
+          user: { id: '1' },
+        },
+        {
+          id: '2',
+          title: 'Tarea 2',
+          description: 'Desc 2',
+          status: 'DONE',
+          user: { id: '1' },
+        },
+      ];
+
+      const mockQueryBuilder = {
+        where: jest.fn().mockReturnThis(),
+        andWhere: jest.fn().mockReturnThis(),
+        getMany: jest.fn().mockImplementation(() => {
+          const filterDto = mockQueryBuilder.filterDto;
+          return Promise.resolve(
+            tasks.filter(
+              (task) =>
+                (!filterDto.status || task.status === filterDto.status) &&
+                (!filterDto.search ||
+                  task.title.includes(filterDto.search) ||
+                  task.description.includes(filterDto.search)),
+            ),
+          );
+        }),
+        filterDto: {} as any,
+      };
+
+      tasksRepository.createQueryBuilder.mockImplementation(
+        () => mockQueryBuilder as any,
+      );
+
+      const user = { id: '1' } as any;
+
+      mockQueryBuilder.filterDto = { status: 'OPEN', search: 'Tarea' };
+      let result = await tasksService.getTasks(
+        mockQueryBuilder.filterDto,
+        user,
+      );
+      expect(result.length).toBe(1);
+      expect(result[0].title).toBe('Tarea 1');
+    });
   });
-  it('returns tasks filtered by status and search', async () => {
-    const tasks = [
-      {
-        id: '1',
-        title: 'Tarea 1',
-        description: 'Desc 1',
-        status: 'OPEN',
+  describe('getTaskById', () => {
+    it('calls getTaskById and return a Task', async () => {
+      const task = {
+        id: '123',
+        title: 'mitarea',
+        status: 'miestado',
+        description: 'midescripcion',
         user: { id: '1' },
-      },
-      {
-        id: '2',
-        title: 'Tarea 2',
-        description: 'Desc 2',
-        status: 'DONE',
-        user: { id: '1' },
-      },
-    ];
+      };
 
-    const mockQueryBuilder = {
-      where: jest.fn().mockReturnThis(),
-      andWhere: jest.fn().mockReturnThis(),
-      getMany: jest.fn().mockImplementation(() => {
-        const filterDto = mockQueryBuilder.filterDto;
-        return Promise.resolve(
-          tasks.filter(
-            (task) =>
-              (!filterDto.status || task.status === filterDto.status) &&
-              (!filterDto.search ||
-                task.title.includes(filterDto.search) ||
-                task.description.includes(filterDto.search)),
-          ),
-        );
-      }),
-      filterDto: {} as any,
-    };
+      tasksRepository.findOne.mockResolvedValue(task);
 
-    tasksRepository.createQueryBuilder.mockImplementation(
-      () => mockQueryBuilder as any,
-    );
+      const result = await tasksService.getTaskById('123', { id: '1' });
 
-    const user = { id: '1' } as any;
-
-    mockQueryBuilder.filterDto = { status: 'OPEN', search: 'Tarea' };
-    let result = await tasksService.getTasks(mockQueryBuilder.filterDto, user);
-    expect(result.length).toBe(1);
-    expect(result[0].title).toBe('Tarea 1');
+      expect(task).toEqual(result);
+    });
   });
 });
